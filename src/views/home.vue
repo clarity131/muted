0e7ecb65a888da6beaf2849160ae2a30d879ba17
clarity <1317798725@qq.com>
2022-05-23 21:57:25 +0800

    修改了样式

--- src/views/home.vue
+++ src/views/home.vue
@@ -81,7 +81,6 @@ a:hover{text-decoration:underline;}
       background-color: rgba(255, 255, 255, .7);
       border-radius: 10px;
       padding: 10px;
-      // width: 500px;
       transition: all .7s;
       margin: 10px;
       .bulletChat {
