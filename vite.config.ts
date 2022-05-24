30abf17f0610aae7b0e0dbfcd94a99ee61335469
ch3cknull <ch3cknull@outlook.com>
2022-05-24 17:37:06 +0800

    chore: add vueuse and auto-import
    Signed-off-by: ch3cknull <ch3cknull@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dts: './src/types/auto-import.ts'
+    })]
 })
